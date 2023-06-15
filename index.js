186bae9feedb244eb72a65795b14bcf12c995b08
Amazing Agu <enyichiaagu@gmail.com>
2023-06-15 16:03:46 +0100

    Filtered habitable planets

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 import fs from 'node:fs'
 import { parse } from 'csv-parse'
 
-const results = []
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	)
+}
 
 fs.createReadStream('kepler_data.csv')
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
 	.on('data', (data) => {
-		results.push(data)
+		if (isHabitablePlanet(data)) {
+			habitablePlanets.push(data)
+		}
 	})
 	.on('error', (err) => {
 		console.log(err)
 	})
 	.on('end', () => {
-		console.log(results)
-		console.log('done')
+		console.log(
+			habitablePlanets.map((planet) => {
+				return planet.kepler_name
+			})
+		)
+		console.log(`${habitablePlanets.length} habitable planets found!`)
 	})
